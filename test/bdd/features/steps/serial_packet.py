53df872e5d957eb0d2d5a6d2de5363412c0118a9
electromantis79 <36968910+electromantis79@users.noreply.github.com>
2018-03-30 15:07:30 -0400

    bdd serial packet

--- test/bdd/features/steps/serial_packet.py
+++ test/bdd/features/steps/serial_packet.py
@@ -8,43 +8,43 @@ use_step_matcher('re')
 def step_impl(context):
 	import app.game.game
 	os.chdir('/Repos/MP2ASCII/app')
-	context.game = app.game.game.Basketball()
+	context.basketball = app.game.game.Basketball()
 
 
 @given("a basketball packet")
 def step_impl(context):
 	import app.serial_IO.serial_packet
-	context.packet = app.serial_IO.serial_packet.SerialPacket(context.game)
+	context.packet = app.serial_IO.serial_packet.SerialPacket(context.basketball)
 
 
 @when('a basketball packet is encoded')
 def step_impl(context):
-	context.string = context.packet.encode_packet()
+	context.basketball_string = context.packet.encode_packet()
 
 
 @then('a basketball packet of the correct format is returned')
 def step_impl(context):
-	print context.string
+	print context.basketball_string
 
 
 @given("a football game")
 def step_impl(context):
 	import app.game.game
 	os.chdir('/Repos/MP2ASCII/app')
-	context.game = app.game.game.Football()
+	context.football = app.game.game.Football()
 
 
 @given("a football packet")
 def step_impl(context):
 	import app.serial_IO.serial_packet
-	context.packet = app.serial_IO.serial_packet.SerialPacket(context.game)
+	context.packet = app.serial_IO.serial_packet.SerialPacket(context.football)
 
 
 @when("a football packet is encoded")
 def step_impl(context):
-	context.string = context.packet.encode_packet()
+	context.football_string = context.packet.encode_packet()
 
 
 @then("a football packet of the correct format is returned")
 def step_impl(context):
-	print context.string
+	print context.football_string
