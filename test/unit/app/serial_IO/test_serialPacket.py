cf850e7c58bd5904b84b9ec83f78edd06617308f
electromantis79 <36968910+electromantis79@users.noreply.github.com>
2018-03-30 17:04:22 -0400

    tdd serial packet

--- test/unit/app/serial_IO/test_serialPacket.py
+++ test/unit/app/serial_IO/test_serialPacket.py
@@ -1,21 +1,154 @@
 from unittest import TestCase
+import app.serial_IO.serial_packet as packet
+import app.game.game as game
+import os
 
 
 class TestSerialPacket(TestCase):
 	def setUp(self):
-		import app.serial_IO.serial_packet as packet
-		import app.game.game as game
-		import os
 		os.chdir('/Repos/MP2ASCII/app')
 		self.game = game.Basketball()
 		self.packet = packet.SerialPacket(self.game)
 
 	def test__string_eater_one_place(self):
-		packet = 'B12345'
+		packet_ = 'B12345'
 		self.assertEquals(
-			self.packet._string_eater(packet, places=1,), '12345')
+			self.packet._string_eater(packet_, places=1,), '12345')
 
 	def test__string_eater_five_places(self):
-		packet = 'B12345'
+		packet_ = 'B12345'
 		self.assertEquals(
-			self.packet._string_eater(packet, places=5,), '5')
+			self.packet._string_eater(packet_, places=5,), '5')
+
+	def test__string_eater_none(self):
+		packet_ = None
+		self.assertIsNone(
+			self.packet._string_eater(packet_, places=5,))
+
+	def _encode_decode_encode(self):
+		print "\nCreate ASCII string"
+		string = self.packet.encode_packet(print_string=True)
+		print "\nLoad String back into game"
+		self.packet.encode_packet(print_string=True, packet=string)
+		print "\nPrint string again to look for changes"
+		final_string = self.packet.encode_packet(print_string=True)
+		self.assertEqual(string, final_string)
+
+	def test_basketball_encode_decode_encode(self):
+		self._encode_decode_encode()
+
+	def _with_vars(self):
+		game_variable_name = 'period'
+		team_variable_name = 'scoreUnits'
+		game_variable = 9
+		team_variable = 6
+		print
+		print game_variable_name, game_variable, 'HOME:', team_variable_name, team_variable
+		self.game.set_game_data(game_variable_name, game_variable, places=1)
+		self.game.set_team_data(self.game.home, team_variable_name, team_variable)
+
+		self._encode_decode_encode()
+
+		self.assertEqual(
+			self.game.get_game_data(game_variable_name), game_variable)
+		self.assertEqual(
+			self.game.get_team_data(self.game.home, team_variable_name), team_variable)
+
+	def test_basketball_encode_decode_encode_with_vars(self):
+		self._with_vars()
+
+	def test_football_encode_decode_encode(self):
+		self.game = game.Football()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_football_encode_decode_encode_with_vars(self):
+		self.game = game.Football()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
+
+	def test_baseball_encode_decode_encode(self):
+		self.game = game.Baseball()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_baseball_encode_decode_encode_with_vars(self):
+		self.game = game.Baseball()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
+
+	def test_soccer_encode_decode_encode(self):
+		self.game = game.Soccer()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_soccer_encode_decode_encode_with_vars(self):
+		self.game = game.Soccer()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
+
+	def test_hockey_encode_decode_encode(self):
+		self.game = game.Hockey()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_hockey_encode_decode_encode_with_vars(self):
+		self.game = game.Hockey()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
+
+	def test_cricket_encode_decode_encode(self):
+		self.game = game.Cricket()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_cricket_encode_decode_encode_with_vars(self):
+		self.game = game.Cricket()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
+
+	def test_racetrack_encode_decode_encode(self):
+		self.game = game.Racetrack()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_racetrack_encode_decode_encode_with_vars(self):
+		self.game = game.Racetrack()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
+
+	def test_stat_encode_decode_encode(self):
+		self.game = game.Stat()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_stat_encode_decode_encode_with_vars(self):
+		self.game = game.Stat()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
+
+	def test_game_encode_decode_encode(self):
+		self.game = game.Game()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._encode_decode_encode()
+
+	def test_game_encode_decode_encode_with_vars(self):
+		self.game = game.Game()
+		self.packet = packet.SerialPacket(self.game)
+
+		self._with_vars()
