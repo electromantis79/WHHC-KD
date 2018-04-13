3ac33f1cdc1ecda92b58ef2420ec30bbedba5fca
electromantis79 <36968910+electromantis79@users.noreply.github.com>
2018-04-13 15:48:21 -0400

    clean and refactor console.py

--- app/Console.py
+++ app/Console.py
@@ -5,7 +5,7 @@
 
 .. topic:: Overview
 
-	This module simulates a console with limited functionality of interpreting MP data.
+	This module simulates a console with the limited functionality of interpreting MP data.
 
 	:Created Date: 3/11/2015
 	:Author: **Craig Gunter**
@@ -17,24 +17,22 @@ import time
 
 import app.utils.functions
 import app.utils.reads
+import app.address_mapping
 import app.mp_data_handler
 import app.serial_IO.serial_packet
-import app.address_mapping
 
 from sys import platform as _platform
 
 
 class Console(object):
 	"""
-	Builds a console object.
-		*Contains verbose comments option*
+	Builds a console object only geared to convert MP data to ASCII data.
 	"""
 
 	def __init__(
-			self, vbose_list=(1, 0, 0), check_events_flag=True,
-			serial_input_flag=0, serial_input_type='MP', serial_output_flag=1, encode_packet_flag=False,
-			server_thread_flag=True):
-		self.className = 'console'
+			self, check_events_flag=True, serial_input_flag=False, serial_input_type='MP',
+			serial_output_flag=True, encode_packet_flag=False, server_thread_flag=False):
+		self.initTime = time.time()
 
 		self.checkEventsFlag = check_events_flag
 		self.serialInputFlag = serial_input_flag
@@ -42,29 +40,49 @@ class Console(object):
 		self.serialOutputFlag = serial_output_flag
 		self.encodePacketFlag = encode_packet_flag
 		self.serverThreadFlag = server_thread_flag
-		self.vboseList = vbose_list
-		self.verbose = self.vboseList[0]  # Method Name or arguments
-		self.verboseMore = self.vboseList[1]  # Deeper loop information in methods
-		self.verboseMost = self.vboseList[2]  # Crazy Deep Stuff
 
-		app.utils.functions.verbose(['\nCreating Console object'], self.verbose)
-
-		self.MP_StreamRefreshFlag = True
+		# Print Flags
+		self.printProductionInfo = True
 		self.printTimesFlag = False
-		self.check_events_active_flag = False
+		self.printInputTimeFlag = False
+		self.verboseDiagnostic = False
+		self.serial_input_verbose_flag = False
+		self.print_check_events_elapse_time_flag = False
+
+		app.utils.functions.verbose(['\nCreating Console object'], self.printProductionInfo)
+
+		# Thread times in seconds
+		self.serialInputRefreshFrequency = 0.1
+		self.serialOutputRefreshFrequency = 0.1
+		self.checkEventsRefreshFrequency = 0.1
+
+		# Variables that don't need resetting through internal reset
+		self.className = 'console'
+		self.serialString = ''
+		self.checkEventsActiveFlag = False
+		self.MP_StreamRefreshFlag = True
 		self.check_events_over_period_flag = False
 		self.count_event_time = 0
 		self.count_event_time_list = []
 		self.ETNSendListCount = 0
 		self.ETNSendListLength = 0
-		self.verboseDiagnostic = False
-		self.print_input_time_flag = False
-		self.serial_input_verbose_flag = False
-		self.print_check_events_elapse_time_flag = False
 		self.count_events_time_flag = False
-		self.initTime = time.time()
-		import datetime
-		print datetime.datetime.now()
+		self.dirtyDict = {}
+		self.sendList = []
+		self.ETNSendList = []
+		self.ETNSendListFlag = False
+
+		self.dataUpdateIndex = 1
+
+		# Main module items set in reset
+		self.configDict = None
+		self.game = None
+		self.addrMap = None
+		self.MPWordDict = None
+		self.previousMPWordDict = None
+		self.mp = None
+		self.sp = None
+		self.priorityListEmech = []
 
 		self.reset()
 
@@ -72,7 +90,7 @@ class Console(object):
 
 	def reset(self, internal_reset=0):
 		"""Resets the console to a new game."""
-		app.utils.functions.verbose(['\nConsole Reset'], self.verbose)
+		app.utils.functions.verbose(['\nConsole Reset'], self.printProductionInfo)
 
 		# Create Game object
 		self.configDict = app.utils.reads.read_config()
@@ -80,44 +98,18 @@ class Console(object):
 			self.game.kill_clock_threads()
 		self.game = app.utils.functions.select_sport_instance(self.configDict, number_of_teams=2)
 
-		print 'sport', self.game.gameData['sport'], 'sportType', self.game.gameData['sportType']
+		app.utils.functions.verbose(
+			['sport', self.game.gameData['sport'], 'sportType', self.game.gameData['sportType']],
+			self.printProductionInfo)
 
-		# Build address maps
 		self.addrMap = app.address_mapping.AddressMapping(game=self.game)
-		self.lampTest = app.address_mapping.LamptestMapping(game=self.game)
-		self.blankTest = app.address_mapping.BlanktestMapping(game=self.game)
-		self.mp = app.mp_data_handler.MpDataHandler()
 		self.addrMap.map()
-
-		# Variables
-		self.dirtyDict = {}
-		self.sendList = []
-		self.ETNSendList = []
-		self.quickKeysPressedList = []
-		self.sendListFlag = False
-		self.ETN_DataFlag = False
-		self.ETNSendListFlag = False
-		self.quantumTunnelFlag = False
-		self.switchKeypadFlag = False
-		self.elapseTimeFlag = False
-		self.busyCheckEventsFlag = True
-		self.keyPressedFlag = False
-		
-		self.broadcastFlag = False
-		self.broadcastString = ''
-		self.showOutputString = False
-		self.sp = app.serial_IO.serial_packet.SerialPacket(self.game)
-		self.serialInputRefreshFrequency = 0.1
-		self.serialOutputRefreshFrequency = 0.1
-		self.checkEventsRefreshFrequency = self.game.gameSettings['periodClockResolution']  # 0.1ms
-		self.serialString = ''
-
 		self.MPWordDict = dict(self.addrMap.wordsDict)
 		self.previousMPWordDict = dict(self.addrMap.wordsDict)
-		self.dataUpdateIndex = 1
-		self.shotClockSportsFlag = (
-				self.game.gameData['sport'] == 'MPBASKETBALL1' or self.game.gameData['sport'] == 'MPHOCKEY_LX1'
-				or self.game.gameData['sport'] == 'MPHOCKEY1')
+
+		self.mp = app.mp_data_handler.MpDataHandler()
+
+		self.sp = app.serial_IO.serial_packet.SerialPacket(self.game)
 
 		self.priorityListEmech = self._select_mp_data_priority()
 
@@ -177,8 +169,10 @@ class Console(object):
 		if _platform == "linux" or _platform == "linux2":
 			print 'Platform is', _platform
 			if self.serialInputFlag and not internal_reset:
-				app.utils.functions.verbose(['\nSerial Input On'], self.verbose)
+				app.utils.functions.verbose(['\nSerial Input On'], self.printProductionInfo)
+
 				import serial_IO.mp_serial
+
 				self.s = serial_IO.mp_serial.MpSerialHandler(
 					serial_input_type=self.serialInputType, game=self.game, verbose_flag=self.serial_input_verbose_flag)
 				self.refresherSerialInput = threading.Thread(
@@ -196,28 +190,8 @@ class Console(object):
 				self.refresherCheckEvents.start()
 
 			if self.serialOutputFlag and not internal_reset:
-				app.utils.functions.verbose(['\nSerial Output On, self.encodePacketFlag', self.encodePacketFlag], self.verbose)
-				self.refresherSerialOutput = threading.Thread(
-					target=app.utils.functions.thread_timer, args=(self._serial_output, self.serialOutputRefreshFrequency))
-				self.refresherSerialOutput.daemon = True
-				self.refresherSerialOutput.name = '_serial_output'
-				self.refresherSerialOutput.start()		
-
-		elif _platform == "darwin":
-			#  OS X
-			print 'Apple Sucks!!!!!', 'Disabling input and output flags'
-			self.serialOutputFlag = False
-			self.serialInputFlag = False
-		elif _platform == "win32":
-			print '\nSerial Input not working for', _platform, 'Disabling input and output flags'
-			self.serialOutputFlag = False
-			self.serialInputFlag = False
-			# self.showOutputString = True
-			if self.checkEventsFlag and not internal_reset:
-				threading.Timer(.1, self._check_events).start()
-				
-			if self.serialOutputFlag and not internal_reset:
-				app.utils.functions.verbose(['\nSerial Output On, self.encodePacketFlag', self.encodePacketFlag], self.verbose)
+				app.utils.functions.verbose(
+					['\nSerial Output On, self.encodePacketFlag', self.encodePacketFlag], self.printProductionInfo)
 				self.refresherSerialOutput = threading.Thread(
 					target=app.utils.functions.thread_timer, args=(self._serial_output, self.serialOutputRefreshFrequency))
 				self.refresherSerialOutput.daemon = True
@@ -227,8 +201,7 @@ class Console(object):
 	# THREADS
 
 	def _serial_input(self):
-		"""Inputs serial packets."""
-		if self.print_input_time_flag:
+		if self.printInputTimeFlag:
 			tic = time.time()
 			init_elapse = tic-self.initTime
 			print '(serial Input %s)' % init_elapse
@@ -236,7 +209,6 @@ class Console(object):
 		self.s.serial_input()
 
 	def _serial_output(self):
-		"""Outputs serial packets."""
 		if self.printTimesFlag or self.verboseDiagnostic:
 			tic = time.time()
 			init_elapse = tic-self.initTime
@@ -244,22 +216,13 @@ class Console(object):
 
 		try:
 			self.s.serial_output(self.serialString)
-			if self.printTimesFlag or self.verboseDiagnostic or self.ETNSendListFlag:
-				pass  # print 'Serial Output', self.serialString
+			if self.verboseDiagnostic:
+				print 'Serial Output', self.serialString
 		except:
 			if not (_platform == "win32" or _platform == "darwin"):
 				print 'Serial Output Error', self.serialString
 
-	# Timer called events for the main program -----------------
-
 	def _check_events(self):
-		"""
-		Checks all events.
-
-		This is called at the checkEventsRefreshFrequency and could be thought of as the main interrupt in a micro-controller.
-
-		The console only updates data for the outside world at this time.
-		"""
 		tic = time.time()
 		if self.printTimesFlag or self.verboseDiagnostic:
 			init_elapse = tic-self.initTime
@@ -270,8 +233,8 @@ class Console(object):
 			self.checkEventsTimer = threading.Timer(self.checkEventsRefreshFrequency, self._check_events).start()
 
 		# This flag is to eliminate double entry to this area (Should never happen anyway)
-		if not self.check_events_active_flag:
-			self.check_events_active_flag = True
+		if not self.checkEventsActiveFlag:
+			self.checkEventsActiveFlag = True
 			
 			# Select normal packet or packet from ETN packet list
 			if self.s.ETNpacketList and not self.ETNSendListFlag:
@@ -280,10 +243,13 @@ class Console(object):
 			else:
 				packet = self.s.packet
 
+			# Save any good data received to the game
 			self.sp.process_packet(print_string=False, packet=packet)
 
+			# Update the new data in addrMap wordDict
 			self._update_mp_words_dict()
 
+			# Prepare data for the output thread
 			self._update_mp_serial_string()
 
 			# Time measurement for testing
@@ -293,6 +259,7 @@ class Console(object):
 				print '_check_events elapse', elapse*1000, ' ms'
 				print
 
+			# Time testing area for finding frequency of over period events
 			if self.count_events_time_flag:
 				self.count_event_time += 1
 				if elapse > self.checkEventsRefreshFrequency:
@@ -301,26 +268,15 @@ class Console(object):
 					self.count_event_time = 0
 				self.check_events_over_period_flag = True
 
-			self.check_events_active_flag = False
+			self.checkEventsActiveFlag = False
 
 		# End Check Events --------------------------------------------------------------------
 
 	def _update_mp_words_dict(self):
-		"""Update address map and MPWordDict and send to drivers if class is scoreboard."""
-		# Select current map
-		if self.game.gameSettings['blankTestFlag']:
-			self.blankTest.map()
-			self.MPWordDict = dict(self.blankTest.wordsDict)
-		elif self.game.gameSettings['lampTestFlag']:
-			self.lampTest.map()
-			self.MPWordDict = dict(self.lampTest.wordsDict)
-		else:
-			self.addrMap.map()
-			self.MPWordDict = dict(self.addrMap.wordsDict)
+		self.addrMap.map()
+		self.MPWordDict = dict(self.addrMap.wordsDict)
 
 	def _update_mp_serial_string(self):
-		"""Updates 18 bytes (9 MP words) into self.sendList and converts it to the self.serialString."""
-
 		# Check for changes in data
 		if cmp(self.MPWordDict, self.previousMPWordDict) != 0:
 			for address in self.MPWordDict.keys():
@@ -414,9 +370,6 @@ class Console(object):
 			serial_string += first_byte+second_byte
 		self.serialString = serial_string
 
-		if self.showOutputString:
-			print self.serialString
-
 	def _custom_send_list_ascii_to_mp_etn(self):
 		if self.sp.ETNChangeFlag:
 			self.sp.ETNChangeFlag = False
@@ -446,16 +399,10 @@ class Console(object):
 
 			self.ETNSendListLength = len(self.ETNSendList)
 
-		if self.ETNSendListFlag:
-			pass  # self.printTimesFlag = True
-		else:
-			pass  # self.printTimesFlag = False
-
 		# Something is in ETN send list and the initial event buffer count is zero
 		# and an over period event has not just happened
 		# Then put one ETN packet in the main send list
 		if self.ETNSendListFlag and self.ETNSendList and not self.check_events_over_period_flag and self.ETNSendListCount < 1:
-			print self.ETNSendList
 			self.sendList = self.ETNSendList[0]
 			self.ETNSendList.pop(0)
 
@@ -472,15 +419,8 @@ class Console(object):
 		name = self.game.get_team_data('TEAM_1', 'name')
 		length = len(name)
 		pairs_list = []
-		team_addr_override = False
 		if length:
-			if self.sp.guestNameChangeOneCharFlag:
-				self.sp.guestNameChangeOneCharFlag = False
-				single_addr = length / 2 + length % 2
-				addr_length = range(abs(length % 2 - 1))
-				team_addr_override = True
-			else:
-				addr_length = range(int(length / 2))
+			addr_length = range(int(length / 2))
 
 			for address in addr_length:
 				left_etn_byte = name[address * 2]
@@ -503,13 +443,8 @@ class Console(object):
 			left_etn_byte = pair[0]
 			right_etn_byte = pair[1]
 
-			if team_addr_override:
-				team_addr = team_addr_shift + single_addr
-			# print 'single team addr'
-			else:
-				team_addr = y + 1 + team_addr_shift
+			team_addr = y + 1 + team_addr_shift
 
-			# print 'normal team addr'
 			# 0xbc = tunnel code
 			# word 2 = address of character pair
 			# word 3 = left_etn_byte
@@ -532,15 +467,8 @@ class Console(object):
 		name = self.game.get_team_data('TEAM_2', 'name')
 		length = len(name)
 		pairs_list = []
-		team_addr_override = False
 		if length:
-			if self.sp.homeNameChangeOneCharFlag:
-				self.sp.homeNameChangeOneCharFlag = False
-				single_addr = length / 2 + length % 2
-				addr_length = range(abs(length % 2 - 1))
-				team_addr_override = True
-			else:
-				addr_length = range(int(length / 2))
+			addr_length = range(int(length / 2))
 
 			for address in addr_length:
 				left_etn_byte = name[address * 2]
@@ -562,12 +490,7 @@ class Console(object):
 		for y, pair in enumerate(pairs_list):
 			left_etn_byte = pair[0]
 			right_etn_byte = pair[1]
-			if team_addr_override:
-				team_addr = team_addr_shift + single_addr
-			# print 'single team addr'
-			else:
-				team_addr = y + 1 + team_addr_shift
-			# print 'normal team addr'
+			team_addr = y + 1 + team_addr_shift
 
 			# 0xbc = tunnel code
 			# word 2 = address of character pair
@@ -595,11 +518,11 @@ class Console(object):
 
 		team_addr_shift = 0
 
-		jword1 = self.mp.encode(2, 4, 1, 0, 1, 10, 13, 0, 0)
-		jword2 = self.mp.encode(2, 4, 2, 0, 0, 0, team_addr_shift, 0, 0, pass3_4_flag=True)
-		jword4 = self.mp.encode(2, 4, 4, 0, 0, 0, font_justify, 0, '', pass3_4_flag=True)
+		word1 = self.mp.encode(2, 4, 1, 0, 1, 10, 13, 0, 0)
+		word2 = self.mp.encode(2, 4, 2, 0, 0, 0, team_addr_shift, 0, 0, pass3_4_flag=True)
+		word4 = self.mp.encode(2, 4, 4, 0, 0, 0, font_justify, 0, '', pass3_4_flag=True)
 
-		send_list = [jword1, jword2, jword4, self.MPWordDict[29], self.MPWordDict[30], self.MPWordDict[32]]
+		send_list = [word1, word2, word4, self.MPWordDict[29], self.MPWordDict[30], self.MPWordDict[32]]
 		self.ETNSendList.append(send_list)
 
 	def _home_font_justify_change(self):
@@ -611,16 +534,16 @@ class Console(object):
 
 		team_addr_shift = 64
 
-		jword1 = self.mp.encode(2, 4, 1, 0, 1, 10, 13, 0, 0)
-		jword2 = self.mp.encode(2, 4, 2, 0, 0, 0, team_addr_shift, 0, 0, pass3_4_flag=True)
-		jword4 = self.mp.encode(2, 4, 4, 0, 0, 0, font_justify, 0, '', pass3_4_flag=True)
+		word1 = self.mp.encode(2, 4, 1, 0, 1, 10, 13, 0, 0)
+		word2 = self.mp.encode(2, 4, 2, 0, 0, 0, team_addr_shift, 0, 0, pass3_4_flag=True)
+		word4 = self.mp.encode(2, 4, 4, 0, 0, 0, font_justify, 0, '', pass3_4_flag=True)
 
-		send_list = [jword1, jword2, jword4, self.MPWordDict[29], self.MPWordDict[30], self.MPWordDict[32]]
+		send_list = [word1, word2, word4, self.MPWordDict[29], self.MPWordDict[30], self.MPWordDict[32]]
 		self.ETNSendList.append(send_list)
 
 
 def test():
-	"""Creates an interpreter."""
+	"""Runs the converter with the sport and jumper settings hardcoded in this function."""
 	print "ON"
 	sport = 'MPBASKETBALL1'
 	c = Config()
@@ -628,7 +551,7 @@ def test():
 	c.write_option_jumpers('0000')
 	Console(
 		check_events_flag=True, serial_input_flag=True, serial_input_type='ASCII',
-		serial_output_flag=True, encode_packet_flag=True, server_thread_flag=True)
+		serial_output_flag=True, encode_packet_flag=True, server_thread_flag=False)
 	while 1:
 		time.sleep(2)
 		# break
